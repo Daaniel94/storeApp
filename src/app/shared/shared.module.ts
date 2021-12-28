36085f68f37c94056da7fa9d6cb2a9d668d48dd7
Luis Daniel Guerrero Ramirez <luisdanielgr94@gmail.com>
2021-12-28 00:38:55 -0600

    footer

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -2,17 +2,19 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MenuComponent } from './menu/menu.component';
 import { PrimeNgModule } from '../prime-ng/prime-ng.module';
-import { MenubarModule } from 'primeng/menubar';
 import { CarouselComponent } from './carousel/carousel.component';
+import { FooterComponent } from './footer/footer.component';
 
 
 
 @NgModule({
   declarations: [
     MenuComponent,
-    CarouselComponent
+    CarouselComponent,
+    FooterComponent
   ],
   exports: [
+    FooterComponent,
     MenuComponent,
     CarouselComponent
   ],
